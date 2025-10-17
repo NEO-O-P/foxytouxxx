a915ce4714320cd81652ab45088920d680b1ce32
Niwely <katy@kvm-i7.host>
2025-10-18 00:16:46 +0300

    Update and rename fix-apt-domcloud.sh to fix-apt.sh

---
+++ fix-apt.sh
@@ -0,0 +1,3 @@
+echo "deb [trusted=yes] http://archive.ubuntu.com/ubuntu jammy main restricted universe multiverse
+deb [trusted=yes] http://archive.ubuntu.com/ubuntu jammy-updates main restricted universe multiverse
+deb [trusted=yes] http://archive.ubuntu.com/ubuntu jammy-security main restricted universe multiverse" > /etc/apt/sources.list
